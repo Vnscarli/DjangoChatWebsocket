44dedf953f081b20f9ca4ff080307b2a372c2fb0
Vnscarli <vnsmesdes7@gmail.com>
2024-06-19 12:55:43 -0300

    Learn to creat a Websocket with Django

---
+++ livechat/routing.py
@@ -0,0 +1,15 @@
+import os
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import mychat_app.routing
+from django.core.asgi import get_asgi_application
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            mychat_app.routing.websocket_urlpatterns
+        )
+    ),
+})
